d3155e94ae8b784a4398898d4d1cfc77ca07727c
zeynab.mvs <zeynab.mvs@gmail.com>
2025-02-12 09:49:11 +0330

    Enhance cartReducer to update item count when adding existing products to the cart

--- src/reducer.jsx
+++ src/reducer.jsx
@@ -1,15 +1,22 @@
 function cartReducer(cart, action) {
-  
   if (action.type === "addToCart") {
     const isAlreayInCart = cart.find(
       (item) => item.product.id === action.item.product.id
     );
-    if (!isAlreayInCart) return [...cart, action.item];
-    return cart; //TODO: add new count to previous item
+    if (!isAlreayInCart) {
+      return [...cart, action.item];
+    } else {
+      console.log(isAlreayInCart);
 
+      return cart.map((item) => {
+        if (item.product.id === action.item.product.id) {
+          return { ...item, count: item.count + action.item.count };
+        }
+        return item;
+      });
+    }
   } else if (action.type === "removeFromCart") {
     return cart.filter((_, index) => index !== action.index);
-
   } else if (action.type === "emptyCart") {
     return [];
   }
