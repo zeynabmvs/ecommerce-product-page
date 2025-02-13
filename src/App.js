e20dac76022f711bba11f53e3b64d444504bbb02
zeynab.mvs <zeynab.mvs@gmail.com>
2025-02-13 09:05:46 +0330

    Refactor: Consolidate multiple product-related components into a single Product component

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
 import Header from "./components/header/Header";
-import Button from "./components/Button";
-import ProductDetails from "./components/ProductDetails";
-import ProductPrice from "./components/ProductPrice";
-import IncrementDecrementButton from "./components/IncDecButton";
-import ProductImage from "./components/ProductImage";
+
 import { product } from "./utils/data";
-import { useState, useReducer, createContext } from "react";
+import { useReducer, createContext } from "react";
 import cartReducer from "./reducer";
+import Product from "./components/Product.jsx";
 
 export const CartContext = createContext();
 
 function App() {
   const [cart, dispatch] = useReducer(cartReducer, []);
-  const [count, setCount] = useState(1);
 
   return (
     <CartContext.Provider value={{ cart, dispatch }}>
-      <div className="App">
+      <div className="app">
         <Header />
-        <main className="container grid grid--1x2">
-          <ProductImage />
-
-          <div>
-            <ProductDetails product={product} />
-            <ProductPrice product={product} />
-            <div className="cs-buttons">
-              <IncrementDecrementButton
-                maxValue={"10"}
-                count={count}
-                setCount={setCount}
-              />
-              <Button
-                name="Add to cart"
-                onClick={() =>
-                  dispatch({
-                    type: "addToCart",
-                    item: {
-                      product: product,
-                      count: count,
-                    },
-                  })
-                }
-              />
-            </div>
-          </div>
+        <main>
+          <Product product={product} />
         </main>
       </div>
     </CartContext.Provider>
