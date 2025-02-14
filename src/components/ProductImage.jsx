c76d19be78ba379bd10cc75470e264e025ee15c8
william2544 <onyangowilliam2002@gmail.com>
2025-02-14 22:35:20 +0300

    Made the Lightbox component

--- src/components/ProductImage.jsx
+++ src/components/ProductImage.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import Icon from "./Icon";
 import "../styles/ProductImage.css";
 import ProductImagesList from "./ProductImagesList.jsx";
+import Lightbox from "./Lightbox.jsx";
 
 const ProductImage = () => {
   const [count, setCount] = useState(1);
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
 
   const images = [1, 2, 3, 4];
 
@@ -29,6 +31,7 @@ const ProductImage = () => {
         <img
           src={process.env.PUBLIC_URL + `/images/image-product-${count}.jpg`}
           alt="Product"
+          onClick={() => setIsLightboxOpen(true)} // Open lightbox on click
         />
         <span className="cs-icon previous-icon" onClick={previousImage}>
           <Icon name="previous" alt="Previous icon" />
@@ -42,6 +45,18 @@ const ProductImage = () => {
         onSetImage={handleSetImage}
         imageCount={count}
       />
+
+      {/* Lightbox (only renders when open) */}
+      {isLightboxOpen && (
+        <Lightbox
+          image={count}
+          images={images}
+          onClose={() => setIsLightboxOpen(false)}
+          onPrev={previousImage}
+          onNext={nextImage}
+          onSetImage={handleSetImage}
+        />
+      )}
     </div>
   );
 };
